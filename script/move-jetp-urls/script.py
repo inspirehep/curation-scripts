f53a76a816c41879ac2b950f3e62ff29fc82ddf4
Micha Moskovic <michamos@gmail.com>
2021-11-10 15:57:56 +0100

    Move JETP URLs

---
+++ script/move-jetp-urls/script.py
@@ -0,0 +1,20 @@
+from inspirehep.curation.search_check_do import SearchCheckDo
+
+
+class MoveJETPURLs(SearchCheckDo):
+    """Move URLs pointing to JETP to new domain."""
+
+    query = 'urls.value:http://www.jetp.ac.ru*'
+
+    @staticmethod
+    def check(record, logger, state):
+        return any(value.startswith("http://www.jetp.ac.ru") value in record.get_value("urls.value", []))
+
+    @statimethod
+    def do(record, logger, state):
+        urls = url.get("urls", [])
+        for url in urls:
+            if url["value"].startswith("http://www.jetp.ac.ru"):
+                url["value"] = url["value"].replace("http://www.jetp.ac.ru", "http://www.jetp.ras.ru")
+
+MoveJETPURLs()
