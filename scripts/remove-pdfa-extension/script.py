ce258ca284b1b1ec2e91ff013711427291c8bafd
Micha Moskovic <michamos@gmail.com>
2021-11-11 10:58:53 +0100

    Remove PDFA extension from filenames (#3)

---
+++ scripts/remove-pdfa-extension/script.py
@@ -0,0 +1,23 @@
+from inspirehep.curation.search_check_do import SearchCheckDo
+
+
+class RemovePDFAExtension(SearchCheckDo):
+    """Remove ``;pdfa`` extension from filenames as it messes up CDS display."""
+
+    query = "documents.filename:*pdfa"
+
+    @staticmethod
+    def check(record, logger, state):
+        return any(
+            f.endswith(";pdfa") for f in record.get_value("documents.filename", [])
+        )
+
+    @staticmethod
+    def do(record, logger, state):
+        extension = ";pdfa"
+        for document in record["documents"]:
+            if (filename := document.get("filename", "")).endswith(extension):
+                document["filename"] = filename[:extension]
+
+
+RemovePDFAExtension()
