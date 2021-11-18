75336fb577f232fffea4cfcd8c0d4465aa99cc6b
hoc3426 <hoc3426@users.noreply.github.com>
2021-11-18 19:36:25 +0000

    Auto-format python code

--- scripts/add-to-fermilab-collection/script.py
+++ scripts/add-to-fermilab-collection/script.py
@@ -2,20 +2,19 @@ from inspirehep.curation.search_check_do import SearchCheckDo
 
 
 class AddToFermilabCollection(SearchCheckDo):
-    '''Add records with Fermilab report numbers to the Fermilab collection.'''
+    """Add records with Fermilab report numbers to the Fermilab collection."""
 
-    query = 'r FERMILAB* -_collections:Fermilab'
+    query = "r FERMILAB* -_collections:Fermilab"
 
     @staticmethod
     def check(record, logger, state):
-        reports = record.get_value('report_numbers.value', [])
+        reports = record.get_value("report_numbers.value", [])
         logger.info("Report numbers in record", reports=reports)
-        return any(
-            report.lower.startswith('fermilab') for report in reports
-        )
+        return any(report.lower.startswith("fermilab") for report in reports)
 
     @staticmethod
     def do(record, logger, state):
-        _collections.append('Fermilab')
+        _collections.append("Fermilab")
+
 
 AddToFermilabCollection()
