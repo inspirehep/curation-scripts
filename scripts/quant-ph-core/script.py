47997871798476125111d9d2563b5ca01b1c6ca8
Micha Moskovic <michamos@gmail.com>
2021-11-24 14:30:42 +0100

    Set quant-ph arXiv category as core (#7)

---
+++ scripts/quant-ph-core/script.py
@@ -0,0 +1,20 @@
+from inspirehep.curation.search_check_do import SearchCheckDo
+from inspirehep.utils import flatten_list
+
+
+class SetQuantPhCore(SearchCheckDo):
+    """Set all papers with quant-ph arXiv category as core."""
+
+    query = "primach quant-ph"
+
+    @staticmethod
+    def check(record, logger, state):
+        categories = flatten_list(record.get_value("arxiv_eprints.categories", []))
+        return any(c == "quant-ph" for c in categories) and not record.get("core")
+
+    @staticmethod
+    def do(record, logger, state):
+        record["core"] = True
+
+
+SetQuantPhCore()
