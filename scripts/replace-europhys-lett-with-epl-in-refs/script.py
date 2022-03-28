0f38a484fea24e562cd02a99a58d42634a4b1fc9
Marcjanna <42305444+MJedr@users.noreply.github.com>
2022-03-28 16:05:23 +0200

    add jobs to replace europhys.lett. with EPL (#9)

---
+++ scripts/replace-europhys-lett-with-epl-in-refs/script.py
@@ -0,0 +1,20 @@
+from inspire_utils.record import get_value
+from inspirehep.curation.search_check_do import SearchCheckDo
+
+
+class ReplaceEurophLetterWithEPLInRefs(SearchCheckDo):
+    """Replace Europhys.Lett. in pubinfo with EPL"""
+
+    query = "references.reference.publication_info.journal_title:europhys.lett."
+
+    @staticmethod
+    def do(record, logger, state):
+        for reference in record["references"]:
+            ref_journal_title = get_value(
+                reference, "reference.publication_info.journal_title", ""
+            )
+            if ref_journal_title.lower() == "europhys.lett.":
+                reference["reference"]["publication_info"]["journal_title"] = "EPL"
+
+
+ReplaceEurophLetterWithEPLInRefs()
