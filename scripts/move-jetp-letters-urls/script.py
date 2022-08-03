5a2386cb972536c3f00a95907a8b82d4c9701d5b
michamos <michamos@users.noreply.github.com>
2022-08-03 09:46:39 +0000

    Auto-format python code

--- scripts/move-jetp-letters-urls/script.py
+++ scripts/move-jetp-letters-urls/script.py
@@ -3,6 +3,7 @@ from inspirehep.curation.search_check_do import SearchCheckDo
 OLD_PREFIX = "http://www.jetpletters.ac.ru"
 NEW_PREFIX = "http://jetpletters.ru"
 
+
 class MoveJETPLettersURLs(SearchCheckDo):
     """Move URLs pointing to JETP Letters to new domain."""
 
@@ -11,8 +12,7 @@ class MoveJETPLettersURLs(SearchCheckDo):
     @staticmethod
     def check(record, logger, state):
         return any(
-            value.startswith(OLD_PREFIX)
-            for value in record.get_value("urls.value", [])
+            value.startswith(OLD_PREFIX) for value in record.get_value("urls.value", [])
         )
 
     @staticmethod
@@ -20,9 +20,7 @@ class MoveJETPLettersURLs(SearchCheckDo):
         urls = record.get("urls", [])
         for url in urls:
             if url["value"].startswith(OLD_PREFIX):
-                url["value"] = url["value"].replace(
-                    OLD_PREFIX, NEW_PREFIX
-                )
+                url["value"] = url["value"].replace(OLD_PREFIX, NEW_PREFIX)
 
 
 MoveJETPLettersURLs()
