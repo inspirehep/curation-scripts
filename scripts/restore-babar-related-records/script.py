4be216622af011fe44309143c64717c170e2c135
Micha Moskovic <michamos@gmail.com>
2022-09-13 17:36:39 +0200

    Dedupe list of related records differently

--- scripts/restore-babar-related-records/script.py
+++ scripts/restore-babar-related-records/script.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from dojson.contrib.marc21.utils import create_record
 from invenio_pidstore.models import PersistentIdentifier
 from inspire_dojson.utils import force_list
+from inspire_utils.dedupers import dedupe_list
 from inspirehep.curation.search_check_do import SearchCheckDo
 from inspirehep.migrator.models import LegacyRecordsMirror
 from inspirehep.records.utils import get_ref_from_pid
@@ -49,9 +50,8 @@ class RestoreBabarRelatedRecords(SearchCheckDo):
 
     @staticmethod
     def check(record, logger, state):
-        recid = record["control_number"]
         try:
-            state["legacy_relations"] = get_legacy_relations(recid)
+            state["legacy_relations"] = get_legacy_relations(record["control_number"])
         except NoResultFound:
             logger.warning("No legacy record found")
             return False
@@ -61,7 +61,7 @@ class RestoreBabarRelatedRecords(SearchCheckDo):
     @staticmethod
     def do(record, logger, state):
         legacy_relations = state["legacy_relations"]
-        seen = set()
+        related_records = []
         for description, report_number in legacy_relations:
             matched_recs = find_report_numbers(report_number)
             if not matched_recs:
@@ -76,17 +76,15 @@ class RestoreBabarRelatedRecords(SearchCheckDo):
                     num_matches=num_matches,
                 )
                 continue
-            description = description.replace("Related", "").strip()
-            if matched_recs[0]["$ref"] in seen:
-                continue
-            seen.add(matched_recs[0]["$ref"])
-            record.setdefault("related_records", []).append(
+            related_records.append(
                 {
                     "relation_freetext": description,
                     "record": matched_recs[0],
                     "curated_relation": True,
                 }
             )
+        if related_records:
+            record["related_records"].append(dedupe_list(related_records))
 
 
 RestoreBabarRelatedRecords()
