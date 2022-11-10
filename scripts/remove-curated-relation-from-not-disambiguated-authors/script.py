4a50602a3c0a26064952914cbf15302a534c0c0e
Marcjanna <42305444+MJedr@users.noreply.github.com>
2022-11-10 11:58:50 +0100

    add script to remove curated_relation when author not disambiguated (#22)

---
+++ scripts/remove-curated-relation-from-not-disambiguated-authors/script.py
@@ -0,0 +1,66 @@
+from inspire_utils.record import get_value
+from inspirehep.curation.search_check_do import SearchCheckDo
+
+
+class RemoveAuthorsCuratedRelation(SearchCheckDo):
+    """Remove curated_relation=True for all the authors that doesn't have record.$ref"""
+
+    query = {
+        "_source": "control_number",
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "nested": {
+                            "path": "authors",
+                            "query": {
+                                "bool": {
+                                    "must_not": {
+                                        "exists": {"field": "authors.record.$ref"}
+                                    },
+                                    "must": {
+                                        "term": {
+                                            "authors.curated_relation": {"value": True}
+                                        }
+                                    },
+                                }
+                            },
+                        }
+                    },
+                    {"match": {"_collections": "Literature"}},
+                ]
+            }
+        },
+    }
+
+    def search(self):
+        self.logger.info("Searching records", query=self.query)
+        query = (
+            self.search_class()
+            .from_dict(self.query)
+            .params(_source={}, size=self.size, scroll="60m")
+        )
+        if shard_filter := self._current_shard_filter():
+            query = query.filter("script", script=shard_filter)
+        return query.scan()
+
+    @staticmethod
+    def check(record, logger, state):
+        no_authors_with_curated_relations = sum(
+            filter(None, get_value(record, "authors.curated_relation", []))
+        )
+        return (
+            len(get_value(record, "authors.record.$ref", []))
+            < no_authors_with_curated_relations
+        )
+
+    @staticmethod
+    def do(record, logger, state):
+        for author in record["authors"]:
+            author_ref = get_value(author, "record.$ref")
+            curated_relation = get_value(author, "curated_relation")
+            if not author_ref and curated_relation:
+                del author["curated_relation"]
+
+
+RemoveAuthorsCuratedRelation()
