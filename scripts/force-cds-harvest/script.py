6daefc2b72726d833c73016ce74c40b6fc988f8b
Micha Moskovic <michamos@gmail.com>
2023-01-25 14:08:52 +0100

    Fix lint error

--- scripts/force-cds-harvest/script.py
+++ scripts/force-cds-harvest/script.py
@@ -5,7 +5,10 @@ from inspirehep.oai.utils import is_cds_set, is_cern_arxiv_set
 class ForceCDSHarvest(SearchCheckDo):
     """Touch records harvested by CDS to force synchronization after fixing bug."""
 
-    query = "(_oai.sets:CERN:arXiv or _oai.sets:ForCDS) -external_system_identifiers.schema:CDS"
+    query = (
+        "(_oai.sets:CERN:arXiv or _oai.sets:ForCDS) "
+        "-external_system_identifiers.schema:CDS"
+    )
 
     @staticmethod
     def check(record, logger, state):
@@ -16,7 +19,8 @@ class ForceCDSHarvest(SearchCheckDo):
 
     @staticmethod
     def do(record, logger, state):
-        # don't need to do anything here, just update the `update` timestamp as a side-effect
+        # don't need to do anything here, just update the `update` timestamp as
+        # a side-effect
         ...
 
 
