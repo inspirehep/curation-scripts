64f9e073a295e8e5f57f611ab0460cc6c2b26f6c
Micha Moskovic <michamos@gmail.com>
2023-01-25 13:58:21 +0100

    Add script to force CDS harvest for some records

---
+++ scripts/force-cds-harvest/script.py
@@ -0,0 +1,20 @@
+from inspirehep.curation.search_check_do import SearchCheckDo
+from inspirehep.oai.utils import is_cds_set, is_cern_arxiv_set
+
+
+class ForceCDSHarvest(SearchCheckDo):
+    """Touch records harvested by CDS to force synchronization after fixing bug."""
+
+    query = '(_oai.sets:CERN:arXiv or _oai.sets:ForCDS) -external_system_identifiers.schema:CDS'
+
+    @staticmethod
+    def check(record, logger, state):
+        is_cds_set(record) or is_cern_arxiv_set(record) or any(id_["schema"] == "CDS" for id_ in record.get("external_system_identifiers", []))
+
+    @staticmethod
+    def do(record, logger, state):
+        # don't need to do anything here, just update the `update` timestamp as a side-effect
+        ...
+
+
+ForceCDSHarvest()
