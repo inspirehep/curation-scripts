69c68fc4e67e2512adabad54580e381e746fd54b
michamos <michamos@users.noreply.github.com>
2023-01-25 12:59:23 +0000

    Auto-format python code

--- scripts/force-cds-harvest/script.py
+++ scripts/force-cds-harvest/script.py
@@ -5,11 +5,14 @@ from inspirehep.oai.utils import is_cds_set, is_cern_arxiv_set
 class ForceCDSHarvest(SearchCheckDo):
     """Touch records harvested by CDS to force synchronization after fixing bug."""
 
-    query = '(_oai.sets:CERN:arXiv or _oai.sets:ForCDS) -external_system_identifiers.schema:CDS'
+    query = "(_oai.sets:CERN:arXiv or _oai.sets:ForCDS) -external_system_identifiers.schema:CDS"
 
     @staticmethod
     def check(record, logger, state):
-        is_cds_set(record) or is_cern_arxiv_set(record) or any(id_["schema"] == "CDS" for id_ in record.get("external_system_identifiers", []))
+        is_cds_set(record) or is_cern_arxiv_set(record) or any(
+            id_["schema"] == "CDS"
+            for id_ in record.get("external_system_identifiers", [])
+        )
 
     @staticmethod
     def do(record, logger, state):
