b6de1ea6f7c48747032f8166c1549cc71858ec3c
Micha Moskovic <michamos@gmail.com>
2023-02-24 11:58:06 +0100

    Improve script to add snowmass URLs

--- scripts/snowmass-add-link/script.py
+++ scripts/snowmass-add-link/script.py
@@ -1,31 +1,32 @@
 from inspirehep.curation.search_check_do import SearchCheckDo
 
+ECONF_URL = 'https://www.slac.stanford.edu/econf/C210711/'
+ECONF_DESCRIPTION = 'eConf'
+ECONF_CNUM = 'C21-07-11'
+
 
 class AddSnowmassProceedingsURL(SearchCheckDo):
     '''Add link to the Snowmass eConf Proceedings website.'''
 
-    econf_url = 'https://www.slac.stanford.edu/econf/C210711/'
-    econf_description = 'eConf'
-    econf_cnum = 'C21-07-11'
 
-    query = f'publication_info.cnum:{econf_cnum}'
-    query += f' -urls.description:{econf_description}'
+    query = f'publication_info.cnum:{ECONF_CNUM}'
+    query += f' -urls.description:{ECONF_DESCRIPTION}'
 
     @staticmethod
     def check(record, logger, state):
         urls = record.get_value('urls.description', [])
         logger.info('URLs in record', urls=urls)
-        if econf_description in urls:
+        if ECONF_DESCRIPTION in urls:
             return False
         cnums = record.get_value('publication_info.cnum', [])
-        return any(cnum.upper() == econf_cnum for cnum in cnums)
+        return ECONF_CNUM in cnums
 
     @staticmethod
     def do(record, logger, state):
-        urls = record.get('urls', [])
-        new_url = {}
-        new_url['value'] = econf_url
-        new_url['description'] = econf_description
-        urls.append(new_url)
+        record.setdefault("urls", []).append({
+            "value": ECONF_URL,
+            "description": ECONF_DESCRIPTION,
+        })
+
 
 AddSnowmassProceedingsURL()
