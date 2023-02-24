770b02c93dfeee8c5df2f7b2d830bf0a3d52ca82
michamos <michamos@users.noreply.github.com>
2023-02-24 11:01:45 +0000

    Auto-format python code

--- scripts/snowmass-add-link/script.py
+++ scripts/snowmass-add-link/script.py
@@ -1,32 +1,33 @@
 from inspirehep.curation.search_check_do import SearchCheckDo
 
-ECONF_URL = 'https://www.slac.stanford.edu/econf/C210711/'
-ECONF_DESCRIPTION = 'eConf'
-ECONF_CNUM = 'C21-07-11'
+ECONF_URL = "https://www.slac.stanford.edu/econf/C210711/"
+ECONF_DESCRIPTION = "eConf"
+ECONF_CNUM = "C21-07-11"
 
 
 class AddSnowmassProceedingsURL(SearchCheckDo):
-    '''Add link to the Snowmass eConf Proceedings website.'''
+    """Add link to the Snowmass eConf Proceedings website."""
 
-
-    query = f'publication_info.cnum:{ECONF_CNUM}'
-    query += f' -urls.description:{ECONF_DESCRIPTION}'
+    query = f"publication_info.cnum:{ECONF_CNUM}"
+    query += f" -urls.description:{ECONF_DESCRIPTION}"
 
     @staticmethod
     def check(record, logger, state):
-        urls = record.get_value('urls.description', [])
-        logger.info('URLs in record', urls=urls)
+        urls = record.get_value("urls.description", [])
+        logger.info("URLs in record", urls=urls)
         if ECONF_DESCRIPTION in urls:
             return False
-        cnums = record.get_value('publication_info.cnum', [])
+        cnums = record.get_value("publication_info.cnum", [])
         return ECONF_CNUM in cnums
 
     @staticmethod
     def do(record, logger, state):
-        record.setdefault("urls", []).append({
-            "value": ECONF_URL,
-            "description": ECONF_DESCRIPTION,
-        })
+        record.setdefault("urls", []).append(
+            {
+                "value": ECONF_URL,
+                "description": ECONF_DESCRIPTION,
+            }
+        )
 
 
 AddSnowmassProceedingsURL()
