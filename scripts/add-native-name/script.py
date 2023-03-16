89542fcbe7e35c58989a66b5de5761b117639876
hoc3426 <hoc3426@users.noreply.github.com>
2023-03-16 21:34:15 +0000

    Auto-format python code

--- scripts/add-native-name/script.py
+++ scripts/add-native-name/script.py
@@ -2,33 +2,33 @@ import re
 from nameparser import HumanName
 from inspirehep.curation.search_check_do import SearchCheckDo
 
-CHINESE_NAME = re.compile(r'[\u4e00-\u9fff]+', re.UNICODE)
+CHINESE_NAME = re.compile(r"[\u4e00-\u9fff]+", re.UNICODE)
+
 
 class AddNativeName(SearchCheckDo):
-    '''Add records with Fermilab report numbers to the Fermilab collection.'''
+    """Add records with Fermilab report numbers to the Fermilab collection."""
 
-    query = '-name.native_names:* _collections:Authors'
+    query = "-name.native_names:* _collections:Authors"
 
     @staticmethod
     def check(record, logger, state):
-        name_dict = record.get_value('name', {})
+        name_dict = record.get_value("name", {})
         logger.info("Names in record", name_dict=name_dict)
-        if 'native_names' in name_dict:
+        if "native_names" in name_dict:
             return False
-        return CHINESE_NAME.search(name_dict['value'])
-
+        return CHINESE_NAME.search(name_dict["value"])
 
     @staticmethod
     def do(record, logger, state):
-        name_dict = record.get_value('name', {})
-        name = name_dict['value']
+        name_dict = record.get_value("name", {})
+        name = name_dict["value"]
         native_name = CHINESE_NAME.search(name).group(0)
-        name = re.sub(native_name, '', name)
+        name = re.sub(native_name, "", name)
         name = HumanName(name)
-        name = f'{name.surnames}, {name.first}'
-        name = re.sub(r'[ ]+\,', ',', name)
-        name_dict['value'] = name
-        name_dict['native_names'] = native_name
+        name = f"{name.surnames}, {name.first}"
+        name = re.sub(r"[ ]+\,", ",", name)
+        name_dict["value"] = name
+        name_dict["native_names"] = native_name
 
 
 AddNativeName()
