813fdfc95cd7ae5ef54bfec485389b8e7bfbbfd2
Micha Moskovic <michamos@gmail.com>
2023-09-25 09:55:48 +0200

    Add script to make internal CDF notes really private (#53)
    * Add script to make internal CDF notes really private
    
    * Auto-format python code
    
    ---------
    
    Co-authored-by: michamos <michamos@users.noreply.github.com>

---
+++ scripts/change-internal-cdf-collection/script.py
@@ -0,0 +1,21 @@
+from inspirehep.curation.search_check_do import SearchCheckDo
+
+
+class ChangeInternalCDFCollection(SearchCheckDo):
+    """Ensure all CDF Internal Notes are really private."""
+
+    query = '_collections:"CDF Internal Notes"'
+
+    @staticmethod
+    def check(record, logger, state):
+        return (
+            len(record["_collections"]) > 1
+            and "CDF Internal Notes" in record["_collections"]
+        )
+
+    @staticmethod
+    def do(record, logger, state):
+        record["_collections"] = ["CDF Internal Notes"]
+
+
+ChangeInternalCDFCollection()
