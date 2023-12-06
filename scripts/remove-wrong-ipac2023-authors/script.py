b501abb0c5e727d1c6bdcd94bb5841c9ff815780
Micha Moskovic <michamos@gmail.com>
2023-12-06 14:13:51 +0100

    Add script to remove wrong IPAC2023 authors

---
+++ scripts/remove-wrong-ipac2023-authors/script.py
@@ -0,0 +1,29 @@
+from inspirehep.curation.search_check_do import SearchCheckDo
+
+CNUM = "C23-05-07"
+AUTHOR_LIST = [
+"Assmann, Ralph",
+"McIntosh, Peter",
+"Fabris, Alessandro",
+"Bisoffi, Giovanni",
+"Andrian, Ivan",
+"Vinicola, Giulia",
+]
+AUTHOR_QUERY = " and ".join(f"a {author}" for author in AUTHOR_LIST)
+
+class RemoveWrongIPAC2023Authors(SearchCheckDo):
+    """Remove incorrect authors on IPAC2023 papers due to bad JACoW metadata."""
+
+    query = f"publication_info.cnum:{CNUM} and {AUTHOR_QUERY}"
+
+    @staticmethod
+    def check(record, logger, state):
+        CNUM in record.get_value("publication_info.cnum", []) and record.get_value("authors.full_name", []) == AUTHOR_LIST
+
+    @staticmethod
+    def do(record, logger, state):
+        del record["authors"]
+
+
+RemoveWrongIPAC2023Authors()
+
