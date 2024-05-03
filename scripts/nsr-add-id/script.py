e3cd58bc68a3c80a3076e0b979de1dcf19de5a19
Micha Moskovic <michamos@gmail.com>
2024-05-03 16:29:25 +0200

    Fix errors in script

--- scripts/nsr-add-id/script.py
+++ scripts/nsr-add-id/script.py
@@ -11,14 +11,15 @@ ELEMENT = "external_system_identifiers"
 
 
 def get_unambiguous_ids(url):
-    """Get a mapping from doi to NSR record ID, ignoring duplicate ambiguous DOIs."""
+    """Get a mapping from doi to NSR record ID, ignoring duplicate DOIs."""
     seen = set()
     result = {}
     data = requests.get(url).json()
     for nsr_id, doi in data:
+        doi = doi.lower()
         if doi in seen:
             result.pop(doi, None)
-        seen.add(doi.lower())
+        seen.add(doi)
         result[doi] = nsr_id
     return result
 
@@ -33,20 +34,22 @@ class AddNSRIds(SearchCheckDo):
 
     @staticmethod
     def check(record, logger, state):
-        dois = record.set_value("dois.value", [])
-        return str(record["control_number"]) in NSR_IDS and not any(
-            id_["schema"] == "NSR"
-            for id_ in record.get("external_system_identifiers", [])
-        )
+        if any(id_["schema"] == "NSR" for id_ in record.get(ELEMENT, [])):
+            return False
+        for doi in record.get_value("dois.value", []):
+            if doi.lower() in NSR_IDS:
+                state[nsr_id] = NSR_IDS[doi.lower()]
+                return True
+        return False
 
     @staticmethod
     def do(record, logger, state):
         record.setdefault(ELEMENT, []).append(
             {
-                "value": NSR_IDS[str(record["control_number"])],
+                "value": NSR_IDS[state[nsr_id]],
                 "schema": "NSR",
             }
         )
 
 
-AddMsnetIds()
+AddNSRIds()
